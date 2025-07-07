4735d6401941254e7de3c28e51a12163bf8a3672
Nilakshi2006 <nilakshibhanot@gmail.com>
2025-07-07 18:41:39 +0530

    keyword

---
+++ day-6/classwork/index.js
@@ -0,0 +1,141 @@
+// document.getElementById("heading").innerText="hey";
+//          document.getElementById("heading").style.border="2px solid red";
+//           document.getElementById("heading").style.width="50%";
+//           document.getElementById("heading").style.padding="20px"
+//           document.getElementById("heading").style.margin="auto"
+//           document.getElementById("heading").style.textAlign="center";
+//         console.log(window);
+//         console.log(document);
+//         console.log("R B")
+
+// //variables
+// //let,const,var
+
+
+// let nilakshi = 1;
+// console.log(nilakshi)
+// nilakshi = 10;
+// const Nilakshi = 2;
+// console.log(Nilakshi)
+// var Nilakshibhanot = 3;
+
+//  let name="Simran";
+//  console.log(name);
+
+
+ 
+//   var url="http://127.0.0.1:5500/day-6/classwork/index.html"
+//   console.log(url)
+
+//number-1,2
+// string-"nilakshi",'nilakshi'
+// boolean-true,false
+// null-
+// undefined
+
+//local/block scop and global scop
+// consider a classroom
+// area inside class is local scop and area outside class is global scop
+//cr follows both means he has acess to both local and global
+//students have only local scop
+//scop tends to area of access
+//let and const has local scop while var has global scop
+
+//assign value in local with let and const only diffrence is that we can resign value in let 
+//but not in const
+
+
+//functions and calling a function
+//tarditional function
+// function myfunction(){   syntax
+//     let a=10;
+//     let b=40;
+//     console.log(a)
+//     // return a+b;
+//     console.log(a+b)
+// }
+// //call a function 
+// myfunction()
+// console.log(a)
+
+// calling function using var
+
+//traditional function
+// myfunction()
+
+// function myfunction(){
+//     let a=10;
+//     let b=40;
+//     console.log(a)
+//     console.log(a+b)
+// }
+// //arrow funtion
+// function myfunction(){
+//     let a=10;
+//     let b=40;
+//     console.log(a)
+//     console.log(a+b)
+// }
+// myfunction()
+
+// console.log(url)
+// var url="http://127.0.0.1:5500/day-6/classwork/index.html"
+// myfunction()
+// console.log(url);
+// const value=90;
+// console.log(value)
+// function myfunction(){
+//     let a=10;
+//     let b=40;
+//     console.log(a+b)
+
+// }
+// console.log(url)
+
+
+// //parameter and argument
+// function myfunction(x,y){ //parameter can be anything
+   
+//     console.log(x+y)
+
+// }
+// let a=30;
+// let b=40;
+// myfunction(a,b);   //a,b is argument.browser takes value of argument
+
+
+// function myfunction(x,y){ //parameter can be anything
+   
+//     console.log(x+" "+y)
+
+// }
+// let a="Rayat";
+// let b="Bahra";
+// myfunction(a,b);     //a,b is argument.browser takes value of argument
+
+// 
+
+//to find length of string
+// let name="Rayat Bahra";
+// console.log(name.length)
+// console.log(length)
+
+
+//to get char and to find length of string
+// let name="Rayat Bahra";
+// let length=name.length;
+// let lastChar=length-1;
+// let firstChar=name[1];
+// console.log(firstChar);
+// console.log(lastChar);
+
+//split a string
+let name="Rayat Bhara";
+let length=name.length;
+let splitChar=name.split(" ");
+console.log(splitChar);
+
+
+  
+
+
